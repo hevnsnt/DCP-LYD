245689562e31bb65750015b83357c2c33b1bbcb1
hevnsnt <hevnsnt@gmail.com>
2015-07-26 15:42:35 -0500

    Slowed down rgbGlitter animation

--- FloorFlowering/FloorFlowering.ino
+++ FloorFlowering/FloorFlowering.ino
@@ -58,7 +58,7 @@ void loop()
   FastLED.show();
   FastLED.delay(1000/FRAMES_PER_SECOND); 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  EVERY_N_MILLISECONDS( 200 ) { gHue++; } // slowly cycle the "base color" through the rainbow
   //EVERY_N_SECONDS( 30 ) { nextPattern(); } // change patterns periodically
 }
 
